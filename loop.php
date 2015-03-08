612184f3433ef2b989ffceff4a561c2600c06ca5
Ben Batschelet <ben@codeup.com>
2015-03-08 17:53:49 -0500

    Expand loop functionality!
    - Usage & help text
    - Can use arbitrary file for input/output
    - Arbitrary delay between iterations (default changed to 5)
    - Iteration limit (makes loop potentially more efficient; save output
      until the end)

--- loop.php
+++ loop.php
@@ -2,15 +2,82 @@
 
 require_once 'queue.class.php';
 
-define('QUEUE_FILE', 'queue.dat');
+$scriptName = array_shift($argv);
 
-$q = new Queue(QUEUE_FILE);
+$usage = <<<USAGE
+Queue Simulator
+
+  Usage:
+    php $scriptName [options]
+
+  Options:
+    -h | --help       : Display this help text
+    -i | --iterations : Limit queue simulation to this number of iterations
+                        Default: unlimited
+    -f | --file       : Use this file to load/save queue data
+                        Default: queue.dat
+    -d | --delay      : Pause this many seconds between iterations
+                        Default: 5
+
+  Description:
+    Queue Simulator emulates a customer qeueue. On each iteration a random
+    number of customers between 1 and 7 will be added to the queue, and some
+    number will be removed. The queue uses sine and cosine functions to
+    replicate a natural rise and fall in customer demand and response. Queue
+    data is cached in a file and will be reloaded at the beginning of execution
+    (if it exists). If no iteration limit is given, queue data is saved on each
+    iteration, otherwise data is saved at the end of the simulation.
+
+
+USAGE;
+
+// Default values
+$filename = 'queue.dat';
+$delay    = 5;
+$limit    = 0;
+$count    = 0;
+
+// Parse commandline options
+$options = getopt('hi:f:d:', array('help', 'iterations:', 'file:', 'delay:'));
+
+foreach ($options as $key => $value) {
+    switch ($key) {
+        case 'h':
+        case 'help':
+            die($usage);
+        case 'i':
+        case 'iterations':
+            if (!ctype_digit($value)) die($usage);
+            $limit = $value;
+            break;
+        case 'f':
+        case 'file':
+            $filename = $value;
+            break;
+        case 'd':
+        case 'delay':
+            if (!ctype_digit($value)) die($usage);
+            $delay = $value;
+            break;
+    }
+}
+
+if (!@touch($filename) || !is_writable($filename)) {
+    die("Cannot write to \"$filename\"!");
+}
+
+// Start the queue!
+$q = new Queue($filename);
 $q->load();
 
-while (true) {
+// If limit is 0 just run forever
+while ($limit === 0 || $count < $limit) {
     $change = $q->tick(time());
 
+    // Default output format for net change
     $chFormat = '%+d';
+
+    // Don't output a sign value if net change is 0
     if ($change == 0) {
         $chFormat = ' %d';
     }
@@ -19,6 +86,12 @@ while (true) {
 
     printf($format, $change, $q->getMax(), $q->getAverage(), $q->getCount(), $q->getTotal());
 
-    $q->save();
-    sleep(1);
+    // Save on each iteration if we're running forever
+    if ($limit === 0) $q->save();
+
+    $count++;
+    sleep($delay);
 }
+
+// Save once loop is complete
+$q->save();
